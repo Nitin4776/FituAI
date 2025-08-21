87957b4de8b179b234a8091b77b8064a8cb6ed2a
Nitin chaudhary <nchaudhary836@gmail.com>
2025-08-21 06:07:34 +0000

    change the firebase configaration used below details - const firebaseCon

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVton6VpvmTbUCyZHmi5dvVkE7JeB5QDE",
-  authDomain: "crickseries.firebaseapp.com",
-  projectId: "crickseries",
-  storageBucket: "crickseries.appspot.com",
-  messagingSenderId: "625037598322",
-  appId: "1:625037598322:web:4d9a5e0c112941869b2ceb"
+  apiKey: "AIzaSyC6tNInkw-ucc4ktW-p5NGtJwgGVk3nhxI",
+  authDomain: "fitlife-ai-u0s3q.firebaseapp.com",
+  projectId: "fitlife-ai-u0s3q",
+  storageBucket: "fitlife-ai-u0s3q.appspot.com",
+  messagingSenderId: "1073563159152",
+  appId: "1:1073563159152:web:d6ac6b3219523bdf6ac2b5"
 };
 
 // Initialize Firebase
