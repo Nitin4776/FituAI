4676fead5bf7001318f0bed44abdb668fbe49c5f
Nitin chaudhary <nchaudhary836@gmail.com>
2025-08-22 11:26:59 +0000

    here is the url for app icon update the same - "https://firebasestorage.

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="https://placehold.co/50x50.png"
+      src="https://firebasestorage.googleapis.com/v0/b/crickseries.firebasestorage.app/o/ChatGPT%20Image%20Aug%2022%2C%202025%2C%2004_53_05%20PM.png?alt=media&token=f683841c-65cc-4887-8590-72f080da56f3"
       alt="App Logo"
       width={40}
       height={40}
