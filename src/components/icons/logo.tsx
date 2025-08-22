951743eb634f93db5b23db6588e18a792b867657
Nitin chaudhary <nchaudhary836@gmail.com>
2025-08-22 12:18:55 +0000

    change the app icon image place holder size make it tripple of current s

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,8 +5,8 @@ export function Logo() {
     <Image
       src="https://firebasestorage.googleapis.com/v0/b/crickseries.firebasestorage.app/o/ChatGPT%20Image%20Aug%2022%2C%202025%2C%2004_53_05%20PM.png?alt=media&token=f683841c-65cc-4887-8590-72f080da56f3"
       alt="App Logo"
-      width={40}
-      height={40}
+      width={120}
+      height={120}
       className="rounded-md"
       data-ai-hint="logo"
     />
