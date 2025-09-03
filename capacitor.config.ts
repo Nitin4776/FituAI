b1055c8ee091cbea0a94ef56c00e6dd7d4dfe997
Nitin chaudhary <nchaudhary836@gmail.com>
2025-09-03 10:52:06 +0000

    app is working in web how to run on android studio?

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,10 @@ const config: CapacitorConfig = {
   appName: 'FituAI',
   webDir: 'out',
   bundledWebRuntime: false,
+  server: {
+    url: 'http://localhost:3000',
+    cleartext: true,
+  },
 };
 
 export default config;
