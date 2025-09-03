0e9259411a4484d6bee31516af75d3b019f6cc04
Nitin chaudhary <nchaudhary836@gmail.com>
2025-09-03 11:00:13 +0000

    npx cap sync android [warn] The bundledWebRuntime configuration option h

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.fitlife.ai',
   appName: 'FituAI',
-  webDir: 'out',
+  webDir: 'public',
   bundledWebRuntime: false,
   server: {
     url: 'http://localhost:3000',
